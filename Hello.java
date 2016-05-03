3acc745da116a17c334810aaceafc3bb834959c5
liuxinxin <liuxinxin1@gmail.com>
2016-05-03 15:30:25 +0800

    update Hello.java Add two lines

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello
 {
 	public static void main(String agrs...){
-		System.out.println("Hello Word !");
+		System.out.println("Hello Word 0001!");
+		System.out.println("Hello Word 0002!");
 	}
 }
