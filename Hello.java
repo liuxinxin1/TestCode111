3474d29aa1e71c209446ebd5162cc8196d428dd3
liuxinxin <liuxinxin1@gmail.com>
2016-05-04 14:54:57 +0800

    提交新代码

--- Hello.java
+++ Hello.java
@@ -4,5 +4,7 @@ public class Hello
 		System.out.println("Hello Word 0001!");
 		System.out.println("Hello Word 0002!");
 		System.out.println("Hello Word 0003!");
+		System.out.println("Hello Word 0002!");
+		System.out.println("Hello Word 0003!");
 	}
 }
